d6e8b7766ae48a0a077e716d71256f8672baca81
Naftali <naftaliholland01@gmail.com>
2023-02-17 22:00:28 -0800

    Program that printd the size of datatypes in c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,6 @@ int main(void)
 	printf("Size of int: %zu byte(s)\n", sizeof(int));
 	printf("Size of long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of float: %lu byte(s)\n", sizeof(float));
+	printf("Size of float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
