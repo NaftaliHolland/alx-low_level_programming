9a30ff65288bc1bf8a77b7a1bf388a20caf52b7e
Naftali <naftaliholland01@gmail.com>
2023-02-17 20:40:38 -0800

    Program that prints a text to the screen using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
 int main(void)
 {
-	puts("Programming is like a building a multilingual puzzle\n");
-	return 0;
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
