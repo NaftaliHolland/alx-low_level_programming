8091f14f09306a85e697e17bff575d7ca18bfb6d
Naftali <naftaliholland01@gmail.com>
2023-02-20 11:01:09 -0800

    Program that prints all the possible combinations of a one digit number

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,20 +4,28 @@
  * Description:Prints numbers
  * Return: Always 0
  */
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints numbers
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	char num;
+	int i;
 
-	for (num = '0'; num <= '9'; num++)
+	for (i = '0'; i <= '9' ; i++)
 	{
-		if (num != '9')
+		putchar(i);
+
+		if (i != '9')
 		{
-			putchar(num);
 			putchar(',');
 			putchar(' ');
 		}
-		else
-			putchar(num);
 	}
+
+	putchar('\n');
 	return (0);
 }
