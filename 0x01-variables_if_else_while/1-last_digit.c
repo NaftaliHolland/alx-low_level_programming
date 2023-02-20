866004d15398ddad489c8fd0dcafdb124a5227ab
Naftali <naftaliholland01@gmail.com>
2023-02-20 06:18:22 -0800

    Program that prints the last digit  of a randomly genrated number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,12 +8,10 @@
  */
 int main(void)
 {
-	int n;
+	int n, u;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int u;
-
 	u = n % 10;
 	if (u > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, u);
