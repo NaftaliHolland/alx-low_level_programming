8e523d1d67ff3f31d8184c53eefbfd08253213bf
Naftali <naftaliholland01@gmail.com>
2023-02-22 18:41:11 -0800

    Program that generates the first 50 fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,18 +6,19 @@
  */
 int main(void)
 {
-	int count, a = 1, b = 2, next;
+	int count;
+	long long int a = 1, b = 2, next;
 
-	printf("%d, %d, ", a, b);
+	printf("%lld, %lld, ", a, b);
 	for (count = 2; count <= 50; count++)
 	{
 		next = a + b;
 		a = b;
 		b = next;
 		if (count != 50)
-		printf("%d, ", next);
+		printf("%lld, ", next);
 		else
-		printf("%d", next);
+		printf("%lld", next);
 	}
 	printf("\n");
 	return (0);
