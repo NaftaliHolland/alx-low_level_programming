a95cb0b4b36d57f306871d5c8c8a0ec57f140f8a
Naftali <naftaliholland01@gmail.com>
2023-02-22 22:30:12 -0800

    Checks whether a character is upercase

---
+++ 0x04-more_functions_nested_loops/0-isuper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isuper - Entry point
+ * Description:Checks whether a character is upercase
+ * @c:Integer it takes as input
+ * Return:1 if true 0 if false
+ */
+int _isuper(int c)
+{
+	char u = c + '0';
+
+	if (u < 'A' || c > 'Z')
+	return (1);
+	else
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+0.Cheks whether a character is upercase
