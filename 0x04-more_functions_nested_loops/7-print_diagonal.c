d184ce9e3821de18c98599bd2b4408bd8e48fd9a
Naftali <naftaliholland01@gmail.com>
2023-02-23 23:50:40 -0800

    Program that prints a diagonal on the terminal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_diagonal - Entry point
+ * Description:Prints a diagonal on the terminal
+ * @n:Number of times \ is to be  printed
+ * Return:void
+ */
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			int s;
+
+			for (s = 0; s < i; s++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\');
+			if (i != n)
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -5,3 +5,4 @@ Prints numbers from 0 -9
 Prints numbers from 0 to 9 except 2 and 4
 Prints 0 to 14 ten times
 Prints a stratight line on the terminal
+Prints diagonal on the terminal
