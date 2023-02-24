79638761a03ca42f55aae084d523dd973a89582a
Naftali <naftaliholland01@gmail.com>
2023-02-23 23:25:04 -0800

    Program that prints a straight line  on the terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - Entry point
+ * Description:Prints a straight line on the terminal
+ * @n:Number of times the character _
+ * return:void
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	else
+		_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -4,3 +4,4 @@ Multiplies two integers
 Prints numbers from 0 -9
 Prints numbers from 0 to 9 except 2 and 4
 Prints 0 to 14 ten times
+Prints a stratight line on the terminal
