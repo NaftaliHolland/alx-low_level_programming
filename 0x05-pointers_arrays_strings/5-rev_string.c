9dc3aa91b25de204225b4a9f5a998be08acc2c84
Naftali <naftaliholland01@gmail.com>
2023-02-28 05:24:59 +0300

    Reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string - Entry point
+ * Description: Reveses a string
+ * @s: input
+ * Return:void
+ */
+void rev_string(char *s)
+{
+	int len, i, half;
+	char tmp;
+
+	for (len = 0; s[len] != '\0'; len++)
+	;
+	i = 0;
+	half = len / 2;
+
+	while (half--)
+	{
+	tmp = s[len - i - 1];
+	s[len - i -1] = s[i];
+	s[i] = tmp;
+	i++;
+	}
+}
