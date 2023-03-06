76aeff87c8bd539eca78851d93bfd3223a76f542
Naftali <naftaliholland01@gmail.com>
2023-03-06 22:37:06 +0300

    A program that fills memory with a single byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memset - Entry point
+ * Description: Fills memory with a constant byte
+ * @s: input
+ * @b: input
+ * @n: input
+ * Return: pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = ; i < (n - 1); i++)
+	{
+		s[i] = b;
+	}
+	
+	return (s);
+}
