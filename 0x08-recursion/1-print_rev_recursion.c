f95862b9d0c2c578dec4ab77960e7b9d2902d163
Naftali <naftaliholland01@gmail.com>
2023-03-08 15:10:22 -0800

    Reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Entry point
+ * Description:Prints string in reverse
+ * @s:String to be printed
+ * Return:void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s +1);
+		_putchar(*s);
+	}
+	else
+		_putchar('\n');
+}
