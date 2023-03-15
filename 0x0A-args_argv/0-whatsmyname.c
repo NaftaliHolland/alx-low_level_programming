dfd9a4de39130a53f7028848491049d38b301433
Naftali <naftaliholland01@gmail.com>
2023-03-15 10:36:37 -0700

    A progrsm that prints it's own name

---
+++ 0x0A-args_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description:Prints it's own name
+ * @argc:Input
+ * @argv:input
+ * Return:int
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
