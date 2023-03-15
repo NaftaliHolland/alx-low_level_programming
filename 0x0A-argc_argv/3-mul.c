85b40d4e3c683504af2f8c25f8c5e3ad18f88b22
Naftali <naftaliholland01@gmail.com>
2023-03-15 11:54:34 -0700

    Multiplys two commandline inputs

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description:multyplyiies two inputs
+ * @argc:Input
+ * @argv:Input
+ * Return:Int
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+}
