72228e5828e1a0972cd199b4c609920b56d8b8ce
Naftali <naftaliholland01@gmail.com>
2023-03-21 11:23:50 -0700

    A struct that contains informationa bout a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Contains inforamtion about a dog
+ * @name: Nameof the dog
+ * @age: Age of the dog
+ * @owner: Name of the owner of the dog
+ * Description: Contains information about a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
