bfc6ee916a1f9bf73c3d10388605d729f32f48f9
Naftali <naftaliholland01@gmail.com>
2023-03-21 13:10:24 -0700

    Function that prints a structs values

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", (*d).name);
 		else
 			printf("Name: (nil)");
-		if (!((*d).age <= 0.0))
+		if (!((*d).age < 0.0))
 			printf("Age: %f\n", (*d).age);
 		else
 			printf("Age: (nil)");
