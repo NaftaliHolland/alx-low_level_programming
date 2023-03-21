706b0c92140f0aa93c588959e073b5284d073d29
Naftali <naftaliholland01@gmail.com>
2023-03-21 12:21:34 -0700

    Function that initolizes a struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*#include "dog.h"*/
+#include "dog.h"
 /**
  * init_dog - Initializes a avariable of type struct dog
  * @d:Struct variable to be initialized
