8f85b671f70cfefb096b03cfac0a55a66bc2e973
Naftali <naftalliholland01@gmail.com>
2023-03-31 06:12:31 +0300

    Added a new line

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * print_list - Prints the ellements of a list
  * @h: pointer to the list
@@ -15,7 +16,7 @@ size_t print_list(const list_t *h)
 		if ((*h).str != NULL)
 			printf("[%u] %s\n", (*h).len, (*h).str);
 		else
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 
 		h = (*h).next;
 		num++;
