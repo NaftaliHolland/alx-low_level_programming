fcc8aa5e9148acb58df2ca0c2586d580cde1a852
Naftali <naftalliholland01@gmail.com>
2023-04-04 19:37:08 +0300

    Changed from recursion to iteration

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,9 +7,12 @@
 
 size_t listint_len(const listint_t *h)
 {
-	if (h == NULL)
+	size_t i = 0;
+	
+	while (h)
 	{
-		return (0);
+		h = (*h).next;
+		i++;
 	}
-	return (1 + listint_len((*h).next));
+	return (i);
 }
