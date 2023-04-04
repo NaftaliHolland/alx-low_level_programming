95c3d67d623385121d00abe0d05b8beb2f8d9b1c
Naftali <naftalliholland01@gmail.com>
2023-04-04 23:57:14 +0300

    Function that pops the first element froma list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * pop_listint - deletes the first node froma a list
+ * @head: pointer to the head node
+ *  Return: Int
+ */
+
+int pop_listint(listint_t **head)
+{
+	int n;
+
+	listint_t *temp = malloc(sizeof(listint_t));
+	temp = *head;
+	head = (*temp).next;
+	n = (*temp).n;
+	free(temp);
+	return(n);
+}
