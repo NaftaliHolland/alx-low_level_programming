81d60325f46f15227b2c5565910cf5fe2e26222a
Naftali <naftalliholland01@gmail.com>
2023-04-04 20:12:30 +0300

    Header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * list_int - Structure for a node
+ *truct listint_s - Structure for a node
  * @n: data
  * @next: pointer to next node
  * listint_t: type definition for listint_s
@@ -18,5 +18,7 @@ typedef struct listint_s
 
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif
