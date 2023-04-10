c823a99ce0ace2d56ffaf47ce88e031f63d53e86
Naftali <naftalliholland01@gmail.com>
2023-04-10 11:05:45 +0300

    A function that prints the binary represenataion of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,33 +10,18 @@
 
 void print_binary(unsigned long int n)
 {
-	/**
-	 *@all_bits: hexadecimal representation of the lesft most bit
-	 * @one: a variable that will tell if 1 has been encountered
-	 */
-	unsigned int all_bits = 0x80000000;
-	int one = 0;
-
 	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
 
-	while (all_bits > 0)
+	if (n == 1)
 	{
-		if ((n & all_bits) != 0)
-			one = 1;
-		if (one == 1)
-		{
-			if ((n & all_bits) == 0)
-				_putchar('0');
-			else
-				_putchar('1');
-		}
-		/**
-		 * Update all_bits to move to the next bit
-		 */
-		all_bits >>= 1;
+		_putchar('1');
+		return;
 	}
+
+	print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
