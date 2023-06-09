898fe3768244f2e084a43cc843aa8403a8086e64
Holland Naftali <naftaliholland01@gmail.com>
2023-06-09 21:25:01 +0300

    A program that prints it's own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argv: array of command line arguments
+ * @argc: Number of command line arguments passed
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
