771294ca901a2180cf6b2faf617843dc1ec5e2af
Naftali Holland <naftaliholland01@gmail.com>
2023-06-19 23:58:49 +0300

    Added a typedef to struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,15 +7,16 @@
  * @age: Age of the dog
  * @owner: Name of the owner
  *
+ * dog_t: typedef for struct dog
  * Description: A structure for dog details
  *
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
