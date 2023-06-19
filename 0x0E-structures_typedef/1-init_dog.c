13d30ae5728ae89d17d86e562073a5460abadae0
Naftali Holland <naftaliholland01@gmail.com>
2023-06-19 23:16:49 +0300

    A finction that initializes the values of a dog struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - Initializes a dog struct
+ *
+ * @d: Pointer to the dog structure to be initialized
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Name of the owner
+ *
+ * Return: void
+ *
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;
+	
+	return;
+}
