62f41969b4f9adbe6bcfc271acb4534099c74960
Naftali Holland <naftaliholland01@gmail.com>
2023-07-04 11:52:55 +0300

    Modified the condition inside the loop to check if the index doesn't exist

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	for (i = 1; i < idx; i++)
 	{
-		if (temp->next == NULL)
+		if (temp == NULL)
 			return (NULL);
 		temp = temp->next;
 	}
