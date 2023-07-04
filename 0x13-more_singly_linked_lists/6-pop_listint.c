1712585e9f383c685541c28c55003d6f72b8c9f0
Naftali Holland <naftaliholland01@gmail.com>
2023-07-04 11:00:50 +0300

    Added check for an empty list

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -14,7 +14,7 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int n;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (0);
 
 	temp = (*head)->next;
@@ -24,5 +24,3 @@ int pop_listint(listint_t **head)
 
 	return (n);
 }
-
-
