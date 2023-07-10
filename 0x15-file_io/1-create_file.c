38a022690ef5453396a4abe9ee95f55e3e74792e
Naftali Holland <naftaliholland01@gmail.com>
2023-07-10 22:40:11 +0300

    Checked if text_content is null before opening thefile

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,6 +20,8 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content == NULL)
+		return (1);
 	i = 0;
 	while (text_content[i] != '\0')
 		i++;
@@ -27,11 +29,6 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_CREAT, 0600);
 	if (fd == -1)
 		return (-1);
-	if (text_content == NULL)
-	{
-		close(fd);
-		return (1);
-	}
 
 	if ((write(fd, text_content, i)) <= 0)
 	{
