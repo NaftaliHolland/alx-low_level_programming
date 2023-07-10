4cc99e2f9f7daa7072fa99aab52582acaea86490
Naftali Holland <naftaliholland01@gmail.com>
2023-07-10 22:34:43 +0300

    Closed the file incase no context was passed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,10 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
+	{
+		close(fd);
 		return (1);
+	}
 
 	if ((write(fd, text_content, i)) <= 0)
 	{
