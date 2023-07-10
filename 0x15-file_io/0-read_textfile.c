e90291cc5e36e6c28112fb3f2db20f26dd6c156e
Naftali Holland <naftaliholland01@gmail.com>
2023-07-10 15:59:30 +0300

    Updated check for malloc's failiure

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-		return (NULL);
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
