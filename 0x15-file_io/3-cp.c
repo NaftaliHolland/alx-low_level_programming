c9a84aa5c21b6b26116021bb992d9522e452f1f7
Holland Naftali Nyadimo <naftaliholland01@gmail.com>
2023-07-29 09:57:42 +0300

    A program that copies the contents of a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,88 @@
+#include "main.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+/**
+ * main - copies the contents of one file to another
+ *
+ * @argc: number of arguments
+ * @argv: pointer to a list of string arguments
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char **argv)
+{
+	int fd_from, fd_to;
+	char *buffer;
+	ssize_t bytes_read;
+
+	if (argc != 3)
+	{
+		dprintf(1, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(1, "Can't read from %s", argv[1]);
+		exit(98);
+	}
+	buffer = malloc(sizeof(char) * BUFFER_SIZE);
+
+	if (buffer == NULL)
+	{
+		close(fd_from);
+		exit(1);
+	}
+	bytes_read = read(fd_from, buffer, BUFFER_SIZE);
+	if (bytes_read == -1)
+	{
+		if ((close(fd_from)) == -1)
+		{
+			dprintf(1, "Can't close fd %d", fd_from);
+			exit(100);
+		}
+		exit(98);
+	}
+	while (bytes_read != 0)
+	{
+		bytes_read = read(fd_from, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			if ((close(fd_from)) == -1)
+			{
+				dprintf(1, "Can't close fd %d", fd_from);
+				exit(100);
+			}
+			exit(98);
+		}
+	}
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		if ((close(fd_from)) == -1)
+		{
+			dprintf(1, "Can't close fd %d", fd_from);
+			exit(100);
+		}
+		dprintf(1, "Error: Can't write to %s", argv[2]);
+		exit(99);
+	}
+
+	dprintf(fd_to, "%s", buffer);
+	if ((close(fd_from)) == -1)
+	{
+		dprintf(1, "Can't close fd %d", fd_from);
+		exit(100);
+	}
+	if ((close(fd_to)) == -1)
+	{
+		dprintf(1, "Can't close fd %d", fd_from);
+		exit(100);
+	}
+	return (0);
+}
