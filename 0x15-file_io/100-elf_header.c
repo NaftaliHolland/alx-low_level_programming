ff17fdbe6b3d388d7650559c0dfdf647ac326249
Holland Naftali Nyadimo <naftaliholland01@gmail.com>
2023-07-29 11:39:42 +0300

    A program that prints elf header, (to be writen later )

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,15 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * main - displays information contained in ELF
+ *
+ * Return: 0 on success
+ *
+ */
+
+int main()
+{
+	printf("ELF\n");
+	return(0):
+}
