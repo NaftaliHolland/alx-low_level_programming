ad8878ba4100d94e8b0204acc12c05c7779e7958
Holland Naftali Nyadimo <naftaliholland01@gmail.com>
2023-10-06 15:01:06 +0300

    A function that adds an element to the hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,48 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * hash_table_set - adds an element to the hash table
+ *
+ * @ht: the hash table you want to add or update the key/value to
+ * @key: the key: connot be empty
+ * @value: the value associated to the key
+ *
+ * Return: 1 on success and 0 on failure
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new, *temp;
+	unsigned long int index;
+
+	if (key == NULL || value == NULL)
+		return (0);
+	/** create the a new node **/
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+
+	new->value = malloc(strlen(value) + 1);
+	if (new->value == NULL)
+		return (0);
+
+	new->key = (char *)key;
+	strcpy(new->value, value);
+
+	/** get the index using the key **/
+	index = key_index((const unsigned char *)key, (unsigned long int)ht->size);
+	temp = ht->array[index];
+	/** insert if list is empty **/
+	if (temp == NULL)
+	{
+		ht->array[index] = new;
+		new->next = NULL;
+		return (1);
+	}
+	/** insert at the begining incase of a collision **/
+	new->next = temp;
+	ht->array[index] = new;
+
+	return (1);
+}
