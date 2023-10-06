0d0e2dd13b3aec64b5c67364af3d44ee157ba56f
Holland Naftali Nyadimo <naftaliholland01@gmail.com>
2023-10-06 15:22:56 +0300

    Added check for null pointer to hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new, *temp;
 	unsigned long int index;
 
-	if (key == NULL || value == NULL)
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 	/** create the a new node **/
 	new = malloc(sizeof(hash_node_t));
